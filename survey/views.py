0e85de8d6f591d6fd9b0f018a281c1f34519d5ae
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-09-04 20:06:32 -0500

    views and urls added to all apps, test of conection whit appscript made

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+import requests
+from rest_framework.decorators import api_view
+from rest_framework import status,response
+from observer.models import Observer
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@api_view(['POST'])
+def resiveResponses(request):
+    print(request.data)
+    return response.Response({"message":"todo bien"}, status=status.HTTP_200_OK)
+
+    
