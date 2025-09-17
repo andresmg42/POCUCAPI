5dbf1a10b4674f702522628cd3afdd42ccd45c4d
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-09-16 22:55:45 -0500

    get_categoria view added to categorya app

--- category/urls.py
+++ category/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("list/", views.get_categories, name="get_categories"),
 ]
--- category/views.py
+++ category/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from category.models import Category
+from category.serializer import CategorySerialier
+from rest_framework.response import Response
+from rest_framework import status,request
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@api_view(['GET'])
+def get_categories(request):
+
+    try:
+
+        categories=Category.objects.all()
+        serializer=CategorySerialier(categories,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({'error':str(e)})
+
+
