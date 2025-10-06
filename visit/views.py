42ff89ac06f7896210696ad6c9dbd7f6b577993d
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-10-06 17:45:40 -0500

    update_start_date function added to update the state and date of visit model

--- visit/views.py
+++ visit/views.py
@@ -4,6 +4,8 @@ from rest_framework import status,response,viewsets
 from .models import Visit
 from .serializer import VisitSerializer
 from surveysession.models import Surveysession
+from django.utils import  timezone
+from response.models import Response
 
 
 
@@ -25,6 +27,34 @@ def get_visits_by_id_session(request):
 
 
     return response.Response(serializer.data,status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def update_start_date(request):
+
+    data=request.data
+
+    visit_id=data['visit_id']
+
+    if not visit_id:
+        return response.Response({'message': 'visit_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+
+        visit=Visit.objects.get(id=visit_id)
+
+        if visit.state==0:
+            visit.state=1
+            visit.visit_start_date_time=timezone.now()
+            visit.save()
+
+
+    except Visit.DoesNotExist:
+        return response.Response({'message':'visit object does not exists'},status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return response.Response({'messge':'an unexpected error occurred in update_start_date'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return response.Response({'message':'visit_start_date_time and state updated successfully'},status=status.HTTP_200_OK)
+
     
 
 class VisitViewSet(viewsets.ModelViewSet):
