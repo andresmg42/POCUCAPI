058c6bc4c064638b8af313a9f21c7fd1ba807a2b
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-10-08 10:26:51 -0500

    custom update function added to surveysession serializer to handle the state when the visit number increase in a update

--- surveysession/serializer.py
+++ surveysession/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Surveysession
 from .models import Surveysession, Zone, Observer, Survey
+from django.utils import timezone
 
 
 class SurveysessionSerializer(serializers.ModelSerializer):
@@ -16,6 +17,7 @@ class SurveysessionSerializer(serializers.ModelSerializer):
         slug_field='number'
     )
 
+    
     # The survey field can still use its ID.
     survey = serializers.PrimaryKeyRelatedField(queryset=Survey.objects.all())
 
@@ -26,3 +28,32 @@ class SurveysessionSerializer(serializers.ModelSerializer):
             'start_date', 'end_date', 'observational_distance', 'url', 'uploaded_at','state','visit_number'
         ]
         read_only_fields = ['uploaded_at']
+
+    def update(self, instance, validated_data):
+
+        instance=super().update(instance,validated_data)
+
+        completed_visits_count=instance.visits.filter(state=2).count()
+
+        if completed_visits_count == instance.visit_number and instance.state != 2:
+            instance.state = 2
+            instance.end_date = timezone.now() 
+            instance.save(update_fields=['state', 'end_date'])
+
+        elif completed_visits_count!=instance.visit_number and instance.state==2:
+            instance.state=1
+            instance.end_date=None
+            instance.save(update_fields=['state','end_date'])
+        return instance
+
+
+
+
+    
+    # def get_state(self,obj):
+
+    #     completed_visits_count=obj.visits.filter(state=2).count()
+
+    #     if completed_visits_count==obj.visit_number:
+    #         return 2
+    #     return obj.state
