00269eed8cf307b9f984a7c1804a3cbea991f390
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-11-10 17:42:04 -0500

    some changes in return data in pocucstats/views.py to standardize the output

--- pocucstats/views.py
+++ pocucstats/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # In your app's views.py
-from django.db.models import Count, Avg, Min, Max,Q
+from django.db.models import Count, Avg, Min, Max,Q,CharField,F
 from rest_framework.views import APIView
 from rest_framework import response,status
 from question.models import Question
@@ -9,6 +9,7 @@ from response.models import Response
 from survey.models import Survey
 from surveysession.models import Surveysession
 from option.models import Option
+from django.db.models.functions import Cast
 
 
 
@@ -73,24 +74,12 @@ class SurveyDashboardView(APIView):
 
         if question.question_type == 'unique_response':
 
-            
-            response_filters = Q(question=question,visita__surveysession__survey_id=survey_id)
+            response_filters = Q(question=question, visita__surveysession__survey_id=survey_id)
             if zone_id:
                 response_filters &= Q(visita__surveysession__zone_id=zone_id)
             response_queryset = Response.objects.filter(response_filters)
 
-            
-            count_filter = Q(response__question=question,response__visita__surveysession__survey_id=survey_id)
-            if zone_id:
-                
-                count_filter &= Q(response__visita__surveysession__zone_id=zone_id)
-            
-            data = question.options.all().annotate(
-                
-                response_count=Count('response', filter=count_filter)
-            ).values('description', 'response_count')
-
-            
+            # 1. Get aggregate stats (this is fine)
             aggregate_data = response_queryset.aggregate(
                 average=Avg('numeric_value'),
                 minimum=Min('numeric_value'),
@@ -99,26 +88,34 @@ class SurveyDashboardView(APIView):
                 count_text=Count('text_value')
             )
 
-           
-            mode_query_number = response_queryset.filter(numeric_value__isnull=False).values('numeric_value').annotate(
-                count=Count('numeric_value')
+            # 2. Define your main 'numeric' query ONCE
+            response_query_number = response_queryset.filter(
+                numeric_value__isnull=False
+            ).annotate(
+                 description=Cast('numeric_value',output_field=CharField())
+            ).values(
+                'description'
+            ).annotate(
+                count=Count('description')
             ).order_by('-count')
 
-            mode_query_text=response_queryset.filter(text_value__isnull=False).values('text_value').annotate(
-                count=Count('text_value')
+            # 3. Define your main 'text' query ONCE
+            response_query_text = response_queryset.filter(
+                text_value__isnull=False
+            ).annotate(
+                 description=F('text_value')
+                 
+            ).values(
+                'description'
+            ).annotate(
+                count=Count('description')
             ).order_by('-count')
 
-            numeric_mode=mode_query_number.first()
-
-            if numeric_mode:
-                mode_result = numeric_mode
-            else:
-                mode_result=mode_query_text.first()
-
-
+            # 4. REUSE those queries
+            aggregate_data['mode_numeric'] = response_query_number.first()
+            aggregate_data['mode_text'] = response_query_text.first()
             
-            aggregate_data['mode'] = mode_result
-            aggregate_data['description']=question.description if len(question.description)<15 else question.code
+            aggregate_data['description'] = question.description if len(question.description) < 15 else question.code
 
             return {
                 'id': question.id,
@@ -126,19 +123,17 @@ class SurveyDashboardView(APIView):
                 'question_code': question.code,
                 'aggregate_stats': [aggregate_data],
                 'visualization_type': 'bar_chart',
-                'data': list(data)
+                # And just slice your existing queries for the data
+                'data_numeric': list(response_query_number[:6]),  # <-- REUSE
+                'data_text': list(response_query_text[:6])        # <-- REUSE
             }
 
         elif question.question_type == 'matrix_parent':
-            final_data = []
+            final_numeric_data = []
+            final_text_data=[]
             final_aggregate_data = []
-            option_descriptions = list(question.options.all().values_list('description', flat=True))
             child_questions = question.child_questions.all().order_by('code')
 
-            
-            count_filter = Q()
-            if zone_id:
-                count_filter = Q(visita__surveysession__zone_id=zone_id)
 
             for child in child_questions:
                 
@@ -148,20 +143,34 @@ class SurveyDashboardView(APIView):
                 child_response_queryset = Response.objects.filter(child_response_filters)
 
                 
-                options_data = {'name': child.description}
-                for desc in option_descriptions:
-                    options_data[desc] = 0
+                options_data_numeric_value = {'name': child.description}
+
+                options_data_text_value={'name':child.description}
+                
 
                 
-                response_counts = Response.objects.filter(child_response_filters).values(
-                    'option__description'
+                response_counts_numeric_value = child_response_queryset.filter(numeric_value__isnull=False).annotate(
+                     description=Cast('numeric_value',output_field=CharField())
+                ).values(
+                    'description'
                 ).annotate(
-                    count=Count('id', filter=count_filter) 
-                )
+                    count=Count('description') 
+                ).order_by('-count')
 
-                for item in response_counts:
-                    if item['option__description']:
-                        options_data[item['option__description']] = item['count']
+                response_counts_text_value = child_response_queryset.filter(text_value__isnull=False).values(
+                    'text_value'
+                ).annotate(
+                    count=Count('text_value') 
+                ).order_by('-count')
+
+                for item in response_counts_numeric_value[:6]:
+                    
+                        options_data_numeric_value[str(item['description'])] = item['count']
+
+                
+                for item in response_counts_text_value[:6]:
+                            
+                        options_data_text_value[item['text_value']]=item['count']
 
                 # 4. Get aggregate stats FOR THIS CHILD.
                 # FIX 4: Removed 'filter=count_filter'. Queryset is already filtered.
@@ -172,27 +181,14 @@ class SurveyDashboardView(APIView):
                     count=Count('numeric_value'),
                     count_text=Count('text_value')
                 )
-
-                mode_query_number = child_response_queryset.filter(numeric_value__isnull=False).values('numeric_value').annotate(
-                count=Count('numeric_value')
-                ).order_by('-count')
-
-                mode_query_text=child_response_queryset.filter(text_value__isnull=False).values('text_value').annotate(
-                    count=Count('text_value')
-                ).order_by('-count')
-
-                numeric_mode=mode_query_number.first()
-
-                if numeric_mode:
-                    mode_result = numeric_mode
-                else:
-                    mode_result=mode_query_text.first()
                 
-                aggregate_data['mode'] = mode_result
+                aggregate_data['mode_numeric'] = response_counts_numeric_value.first()
+                aggregate_data['mode_text']= response_counts_text_value.first()
                 aggregate_data['description'] = child.description 
 
-                
-                final_data.append(options_data)
+                final_numeric_data.append(options_data_numeric_value)
+                if len(list(response_counts_text_value))!=0:
+                    final_text_data.append(options_data_text_value)
                 final_aggregate_data.append(aggregate_data)
 
             return {
@@ -201,7 +197,8 @@ class SurveyDashboardView(APIView):
                 'question_code': question.code,
                 'visualization_type': 'stacked_bar_100_percent',
                 'aggregate_stats': final_aggregate_data,
-                'data': final_data
+                'data_numeric': final_numeric_data,
+                'data_text':final_text_data
             }
                      
         
