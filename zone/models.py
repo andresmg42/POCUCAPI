ca1a72726d21aaab11ee577082fcb28cf7ba47e9
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-11-25 14:22:43 -0500

    zone name text  size increased

--- zone/models.py
+++ zone/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Zone(models.Model):
-    name=models.CharField(max_length=30)
+    name=models.CharField(max_length=100)
     number=models.IntegerField(default=1)
 
     def __str__(self):
