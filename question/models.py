f314608e48f460ecf605c036c179a1ca9c12cb45
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-12-05 17:27:05 -0500

    input_type atribute added to question model

--- question/models.py
+++ question/models.py
@@ -6,6 +6,11 @@ from option.models import Option
 
 
 class Question(models.Model):
+
+    class InputType(models.TextChoices):
+        NUMERIC='NUM','Numerico'
+        TEXT='STR','Textual'
+
     subcategory=models.ForeignKey(Subcategory,on_delete=models.CASCADE)
     options = models.ManyToManyField(
         Option,
@@ -18,6 +23,10 @@ class Question(models.Model):
     parent_question=models.ForeignKey('self', on_delete=models.CASCADE,null=True,blank=True,related_name='child_questions')
     survey=models.ManyToManyField(Survey,related_name='questions')
     is_required=models.BooleanField(default=True)
+    input_type=models.CharField(
+        max_length=3,
+        choices=InputType.choices,
+        default=InputType.NUMERIC)
     position = models.FloatField(
         default=1.0, 
         help_text="The order of the question (e.g., 1.0, 1.5, 2.0)"
