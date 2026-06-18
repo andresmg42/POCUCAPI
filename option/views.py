201a8fa550ff8ac159f3c2a8a05ba6c8b514a71b
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2026-06-17 19:23:43 -0500

    atribute type and unique description and type fields constraint added to option model

--- option/views.py
+++ option/views.py
@@ -7,3 +7,11 @@ class OptionViewSet(viewsets.ModelViewSet):
 
     queryset = Option.objects.all()
     serializer_class = OptionSerializer
+    def get_queryset(self):
+        queryset=Option.objects.all()
+
+        matching_type=self.request.query_params.get('matching_type')
+        if matching_type:
+            return queryset.filter(type=matching_type)
+        return queryset
+        
